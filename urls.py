170d2729cbee56812775dec81a0b485a821736a1
kioopi <kioopi@gmail.com>
2009-08-05 00:43:13 +0200

    modifications to accomodate mod_wsgi deployment

--- urls.py
+++ urls.py
@@ -13,7 +13,8 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/(.*)', admin.site.root),
 
     (r'^termine/', include('plotter2010.apps.termine.urls')),
     (r'^gruppen/', include('plotter2010.apps.organizers.urls')),
--- wsgi_handler.py
+++ wsgi_handler.py
@@ -1,7 +1,9 @@
 import os, sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'plotter.settings'
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'')
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/..')
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/apps')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'plotter2010.settings'
 
 import django.core.handlers.wsgi
 
