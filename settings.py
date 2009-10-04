1086c83bd16bb2b84a14dbc10d2e99676a6a3bab
kioopi <kioopi@gmail.com>
2009-10-04 18:48:16 +0200

    added flatpages.

--- settings.py
+++ settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'plotter2010.urls'
@@ -83,6 +84,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'django.contrib.flatpages',
     'django_extensions',
     'south', 
     'organizers',
