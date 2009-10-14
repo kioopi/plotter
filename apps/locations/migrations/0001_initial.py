b1493bcc16af95c3a0c4550f9dddd49d3b5553ec
kioopi <kioopi@gmail.com>
2009-10-14 02:36:20 +0200

    commited the test-sqlite. just to mess more with the migrations.

--- apps/locations/migrations/0001_initial.py
+++ apps/locations/migrations/0001_initial.py
@@ -9,27 +9,27 @@ class Migration:
         
         # Adding model 'City'
         db.create_table('locations_city', (
-            ('id', models.AutoField(primary_key=True)),
-            ('name', models.CharField(max_length=100)),
-            ('slug', models.SlugField(max_length=100)),
-            ('lat', models.FloatField(null=True, blank=True)),
-            ('long', models.FloatField(null=True, blank=True)),
-            ('text', models.TextField(null=True, blank=True)),
+            ('id', orm['locations.City:id']),
+            ('name', orm['locations.City:name']),
+            ('slug', orm['locations.City:slug']),
+            ('lat', orm['locations.City:lat']),
+            ('long', orm['locations.City:long']),
+            ('text', orm['locations.City:text']),
         ))
         db.send_create_signal('locations', ['City'])
         
         # Adding model 'Location'
         db.create_table('locations_location', (
-            ('id', models.AutoField(primary_key=True)),
-            ('name', models.CharField(max_length=100)),
-            ('slug', models.SlugField()),
-            ('city', models.ForeignKey(orm.City)),
-            ('adress', models.CharField("Adresse", max_length=250, blank=True)),
-            ('zipcode', models.CharField("PLZ", max_length=20, blank=True)),
-            ('lat', models.FloatField(null=True, blank=True)),
-            ('long', models.FloatField(null=True, blank=True)),
-            ('display_in_list', models.BooleanField("Anzeigen", default=True)),
-            ('text', models.TextField(null=True, blank=True)),
+            ('id', orm['locations.Location:id']),
+            ('name', orm['locations.Location:name']),
+            ('slug', orm['locations.Location:slug']),
+            ('city', orm['locations.Location:city']),
+            ('adress', orm['locations.Location:adress']),
+            ('zipcode', orm['locations.Location:zipcode']),
+            ('lat', orm['locations.Location:lat']),
+            ('long', orm['locations.Location:long']),
+            ('display_in_list', orm['locations.Location:display_in_list']),
+            ('text', orm['locations.Location:text']),
         ))
         db.send_create_signal('locations', ['Location'])
         
@@ -47,24 +47,24 @@ class Migration:
     
     models = {
         'locations.city': {
-            'id': ('models.AutoField', [], {'primary_key': 'True'}),
-            'lat': ('models.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'long': ('models.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'name': ('models.CharField', [], {'max_length': '100'}),
-            'slug': ('models.SlugField', [], {'max_length': '100'}),
-            'text': ('models.TextField', [], {'null': 'True', 'blank': 'True'})
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lat': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'long': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100', 'db_index': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
         },
         'locations.location': {
-            'adress': ('models.CharField', ['"Adresse"'], {'max_length': '250', 'blank': 'True'}),
-            'city': ('models.ForeignKey', ["orm['locations.City']"], {}),
-            'display_in_list': ('models.BooleanField', ['"Anzeigen"'], {'default': 'True'}),
-            'id': ('models.AutoField', [], {'primary_key': 'True'}),
-            'lat': ('models.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'long': ('models.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'name': ('models.CharField', [], {'max_length': '100'}),
-            'slug': ('models.SlugField', [], {}),
-            'text': ('models.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'zipcode': ('models.CharField', ['"PLZ"'], {'max_length': '20', 'blank': 'True'})
+            'adress': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['locations.City']"}),
+            'display_in_list': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lat': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'long': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'})
         }
     }
     
